5a0d71ee437f109ff7c296eed43cd011af12ca33
Martini Luca <l.martini@nwgenergia.it>
2024-11-22 11:16:42 +0100

    remove space

--- configuration.nix
+++ configuration.nix
@@ -1,50 +1,50 @@
 { pkgs, self, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.zsh
-        ];
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ 
+      pkgs.zsh
+    ];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
-      nix.extraOptions = 
-      ''
-        extra-platforms = x86_64-darwin aarch64-darwin
-      '';
+  nix.extraOptions = 
+  ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
 
-      system.defaults.finder = {
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-        ShowPathbar = true;
-        _FXSortFoldersFirst = true;
-      };
-      system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
-      system.defaults.screencapture.location = "~/Screenshots";
-      security.pam.enableSudoTouchIdAuth = true;
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    ShowPathbar = true;
+    _FXSortFoldersFirst = true;
+  };
+  system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+  system.defaults.screencapture.location = "~/Screenshots";
+  security.pam.enableSudoTouchIdAuth = true;
 
-      homebrew = {
-        enable = true;
-        brews = [
-          "pulumi"
-        ];
-        casks = [
-          "wezterm@nightly"
-        ];
-      };
-    }
+  homebrew = {
+    enable = true;
+    brews = [
+      "pulumi"
+    ];
+    casks = [
+      "wezterm@nightly"
+    ];
+  };
+}
